cfebf44fd454a3ce84d27e053340546591f6ac3d
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2022-01-14 22:06:45 -0300

    style(about): altera layout da tela sobre

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -29,47 +29,42 @@
         <h2 class="text-center service-text">Conheça melhor nossos serviços</h2>
     </div>
 
-    <div class="row">
-        <div class="col-12 col-sm-12 col-md-4">
-            <div class="card">
-                <img src="/assets/img/miniature/Cilios.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <h5 class="card-title">Cilios</h5>
-                    <p class="card-text">
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
+    <div class="row about-section image-text">
+        <div class="col image"><img src="https://picsum.photos/400/300" alt="..."></div>
+        <div class="col text">
+            <h2>Lorem Ipsum</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac sapien vel purus
+                eleifend euismod. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                Phasellus tincidunt turpis iaculis, tempus dolor ornare, molestie lectus. Quisque accumsan enim purus, vitae
+                scelerisque sapien sagittis at.
+            </p>
         </div>
+    </div>
 
-        <div class="col-12 col-sm-12 col-md-4">
-            <div class="card">
-                <img src="/assets/img/miniature/Sobrancelha.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <h5 class="card-title">Sobrancelha</h5>
-                    <p class="card-text">
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
+    <div class="row about-section text-image">
+        <div class="col text">
+            <h2>Lorem Ipsum</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac sapien vel purus
+                eleifend euismod. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                Phasellus tincidunt turpis iaculis, tempus dolor ornare, molestie lectus. Quisque accumsan enim purus, vitae
+                scelerisque sapien sagittis at.
+            </p>
         </div>
+        <div class="col image"><img src="https://picsum.photos/400/300" alt="..."></div>
+    </div>
 
-        <div class="col-12 col-sm-12 col-md-4">
-            <div class="card">
-                <img src="/assets/img/miniature/Maquiagem.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
+    <div class="row about-section image-text">
+        <div class="col image"><img src="https://picsum.photos/400/300" alt="..."></div>
+        <div class="col text">
+            <h2>Lorem Ipsum</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac sapien vel purus
+                eleifend euismod. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                Phasellus tincidunt turpis iaculis, tempus dolor ornare, molestie lectus. Quisque accumsan enim purus, vitae
+                scelerisque sapien sagittis at.
+            </p>
         </div>
     </div>
 @endsection
