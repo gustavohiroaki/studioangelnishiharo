e74b5ff05c4a9a92a6aefdbedc0a67bd8a81aca1
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2022-02-16 21:47:51 -0300

    style(home): altera backgrounds

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,62 +1,57 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="/assets/css/main.min.css" />
     <link rel="stylesheet" href="/assets/css/home.min.css" />
     <title>Studio Angel Nishiharo</title>
-  </head>
-  <body class="container hero">
+</head>
+
+<body class="container hero">
+    <img id="angelica" src="/assets/img/angelica.png" alt="Angélica Nishiharo">
     <nav class="navbar navbar-expand-lg navbar-light">
-      <div class="container d-flex justify-content-between">
-        <a class="navbar-brand" href="#">
-          <img src="/assets/img/logo.png" alt="" />
-        </a>
-        <div>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/sobre">Sobre</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Serviços</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Contato</a>
-              </li>
-            </ul>
-          </div>
+        <div class="container d-flex justify-content-between">
+            <a class="navbar-brand" href="#">
+                <img src="/assets/img/logo.png" alt="" />
+            </a>
+            <div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/sobre">Sobre</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Serviços</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contato</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
     </nav>
 
     <div class="hero-container">
-      <div class="hero-content d-flex flex-column align-items-center">
-        <h2>Beleza na medida certa</h2>
-        <button type="button" class="btn btn-primary">Conheça Mais</button>
-      </div>
+        <div class="hero-content d-flex flex-column align-items-center">
+            <h2>Beleza na medida certa</h2>
+            <button type="button" class="btn btn-primary">Conheça Mais</button>
+        </div>
     </div>
 
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-      crossorigin="anonymous"
-    ></script>
-  </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</body>
+
 </html>
