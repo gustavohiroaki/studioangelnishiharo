439ecfcd7d9b5dad29dcda6b5d80b7b65a8a521c
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2022-03-01 15:51:10 -0300

    feat(splash): adiciona splash screen para tela inicial

---
+++ public/assets/js/splash.js
@@ -0,0 +1,35 @@
+let alreadyShowed = showedCookieExists();
+let splash = document.querySelector("#splash");
+let studioName = document.querySelector(".studio-name");
+let nameFragment = document.querySelectorAll(".name-fragment");
+
+function showed() {
+    var data = new Date();
+    data.setMinutes(data.getMinutes() + 10);
+    data = data.toGMTString();
+    document.cookie = "showed=true" + "; expires=" + data + "; path=/";
+}
+
+function showedCookieExists() {
+    var cname = " " + "showed" + "=";
+    var cookies = document.cookie;
+
+    return cookies.indexOf(cname) !== -1;
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    if (!alreadyShowed) {
+        splash.classList.add("active");
+        setTimeout(() => {
+            nameFragment.forEach((span, index) => {
+                setTimeout(() => {
+                    span.classList.add("active");
+                }, (index + 1) * 400);
+            });
+            setTimeout(() => {
+                splash.style.top = "-100vh";
+            }, 3000);
+        });
+        showed();
+    }
+});
